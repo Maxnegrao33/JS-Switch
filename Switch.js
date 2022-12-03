f8c5899cfc95963fe72e79bc18a211be4a5d1eab
Maciel Ribeiro de Oliveira <38005099+Maxnegrao33@users.noreply.github.com>
2022-12-03 12:08:28 -0300

    Add files via upload

---
+++ Switch.js
@@ -0,0 +1,25 @@
+let profession = "fiscal";
+
+    console.log("Profissão: " + profession);
+
+    switch(profession) {
+        case "fiscal":
+            console.log("Sua camisa será: VERDE");
+            break;
+        case "bombeiro":
+            console.log("Sua camisa será: VERMELHA");
+            break;
+        case "policial":
+            console.log("Sua camisa será: CINZA");
+            break;
+        default:
+            console.log("Sua camisa será: PRETA");
+            break;
+    }
+
+//o break faz com que a verificação pare, quando a condicional for satisfeita
+
+
+
+
+
